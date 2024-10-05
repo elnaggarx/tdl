94febff8a13379fbead65db0e78f09e113a032c3
Mohamed Amr <mohamedamr2014.ma@gmail.com>
2024-10-05 21:28:51 +0300

    m

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var id=0;
+var b=false;
+function toggleAddTaskMenu(){
+
+        document.getElementById("popup").classList.toggle("hide");
+}
+function addTask(){
+    var inputFieldValue = document.getElementById("task-input").value;
+    var newListItem  =document.createElement("li");
+    newListItem.setAttribute(`id` , `item-${id}`);
+    newListItem.innerHTML=
+    `<div>
+        <div class="task-check-container">
+            <div class="checkbox-wrapper-13">
+                <input id="c1-13" type="checkbox">
+            </div>
+            <p id="task-${id}">${inputFieldValue}</p>
+        </div>
+        <div class="edit-delete-container">
+            <button class="edit-button edit-delete" id="edit-${id}" onclick="edit(${id})">Edit</button>
+            <button class="delete-button edit-delete" onclick="deleteItem(${id})">Delete</button>
+        </div>
+
+    </div>`;
+    document.getElementById("tasks-list").appendChild(newListItem);
+    toggleAddTaskMenu();
+    id++;
+    
+}
+function edit(id){
+    b=!b;
+   document.getElementById(`task-${id}`).setAttribute("contenteditable" , b);
+   if(b===true){
+    document.getElementById(`edit-${id}`).innerHTML = "Done"
+   }
+   else{
+    document.getElementById(`edit-${id}`).innerHTML = "Edit"
+   }
+}
+function deleteItem(id){
+    document.getElementById(`item-${id}`).remove();
+}
